153835c5129b2ef9d0df25795ade4148d09c1594
SaprykinSemen <saprykinsemendp@gmail.com>
2017-08-07 09:52:02 +0300

    1

---
+++ baby-born/js/custom.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+
+	
+
+	$('.header-menu-btn').on('click', function() {	
+	$('.header-nav-wrap').toggle();
+});
+
+	$('.location-link').magnificPopup({
+		type:'inline',
+		midClick: true,
+		alignTop: true,
+	});
+	
+	$('.header-callback').magnificPopup({
+		type:'inline',
+		midClick: true,
+		alignTop: true,
+	});
+
+	$('.header-enter-link').magnificPopup({
+		type:'inline',
+		midClick: true,
+		alignTop: true,
+	});
+	
+	$('.header-enter-link-registration').magnificPopup({
+		type:'inline',
+		midClick: true,
+		alignTop: true,
+	});
+
+	$('.registration-form-btn').magnificPopup({
+  items: {
+      src: '.thank-registration-popup',
+      type: 'inline'
+  },
+  closeBtnInside: true
+});
+
+	$('.main-slider').bxSlider({
+		pager: false,
+		prevText: "",
+		prevSelector: ".main-prev",
+		nextText: "",
+		nextSelector: ".main-next",
+	});
+
+	$('.sale-slider').bxSlider({
+		auto: true,
+		pause: 3000,
+		autoHover: true,
+		controls: false,
+	});
+
+});
